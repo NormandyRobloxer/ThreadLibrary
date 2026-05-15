49358a48039031a89c3fe329d4055e6371998b59
NormandyRobloxer <168982043+NormandyRobloxer@users.noreply.github.com>
2026-05-15 17:25:10 +0900

    Update Thread.lua

--- Thread.lua
+++ Thread.lua
@@ -3,60 +3,58 @@ local Thread = {}
 Thread._threads = {}
 
 function Thread.new(name, fn)
-	assert(type(name) == "string", "name must be string")
-	assert(type(fn) == "function", "fn must be function")
+	assert(type(name) == "string")
+	assert(type(fn) == "function")
 
 	if Thread._threads[name] then
-		error(("thread '%s' already exists"):format(name))
+		error("thread already exists")
 	end
 
 	Thread._threads[name] = {
 		Name = name,
 		Function = fn,
-		Alive = false
+		Running = false
 	}
 end
 
-function Thread.start(name)
-	local thread = Thread._threads[name]
+function Thread.start(name, ...)
+	local t = Thread._threads[name]
+	if not t or t.Running then return end
 
-	if not thread or thread.Alive then
-		return
-	end
+	t.Running = true
 
-	thread.Alive = true
-
-	task.spawn(function()
-		while thread.Alive do
-			thread.Function()
-			task.wait()
-		end
-	end)
+	task.spawn(function(...)
+		t.Function(...)
+		t.Running = false
+	end, ...)
 end
 
 function Thread.stop(name)
-	local thread = Thread._threads[name]
+	local t = Thread._threads[name]
+	if not t then return end
 
-	if not thread then
-		return
-	end
-
-	thread.Alive = false
+	t.Running = false
 end
 
-function Thread.remove(name)
-	local thread = Thread._threads[name]
+function Thread.join(name)
+	local t = Thread._threads[name]
+	if not t then return end
 
-	if not thread then
-		return
+	while t.Running do
+		task.wait()
 	end
-
-	thread.Alive = false
-	Thread._threads[name] = nil
 end
 
 function Thread.exists(name)
 	return Thread._threads[name] ~= nil
+end
+
+function Thread.remove(name)
+	local t = Thread._threads[name]
+	if not t then return end
+
+	t.Running = false
+	Thread._threads[name] = nil
 end
 
 return Thread
